6743cf519c2e25b75ac209f03896904b0ba5133c
Jiawei Li <iewaijil@gmail.com>
2022-03-28 18:12:53 +0200

    Update factors

--- qs.py
+++ qs.py
@@ -1,21 +1,8 @@
 import pandas as pd
 
-class Dataset:
-    """
-    Base class for downloading dataset.
-
-    Args:
-        root: Root directory of dataset
-    """
-    def __init__(self, root, cache = True):
-        self.root = root
-        if cache:
-            self.cache()
 
 def cache(func):
     def wrapped_func(*args, **kwargs):
-        from datetime import date
-
         table_name = func.__name__.split("_")[-1]
         parquet_path = f"./data/{table_name}.parquet"
         try:
@@ -34,20 +21,59 @@ def query(sql_stmt, params, wrds_username):
         data = db.raw_sql(sql_stmt, date_cols=["date"], params=params)
     return data
 
-@cache
 def get_crsp(permnos, wrds_username):
     sql_crsp = """
-    SELECT DISTINCT
-      date,
-      permno,
-      ret,
-      prc,
-      prc * shrout AS cap
-    FROM crsp.dsf
-    WHERE permno in %(permnos)s
-    AND date >= '1997-01-01'
-    AND date <= '2019-12-31';
+    SELECT
+        date,
+        permno,
+        openprc AS open,
+        askhi AS high,
+        bidlo AS low,
+        prc AS close,
+        vol AS volume,
+        ret,
+        shrout
+    FROM
+        crsp.dsf
+    WHERE
+        permno IN %(permnos)s
+        AND date >= '1994-01-01'
+        AND date <= '2019-12-31'
+    ORDER BY
+        date, permno;
     """
     params = {"permnos": permnos}
-    crsp = query(sql_crsp, params, wrds_username).astype({"permno":"int"})
+    crsp = query(sql_crsp, params, wrds_username)
+    # Fill missing close prices of permno 80539
+    crsp.loc[crsp.permno == 80539, "close"] = crsp.loc[crsp.permno == 80539, "close"].fillna(method="ffill")
+    # Fill other missing values
+    crsp = crsp.fillna({"open": crsp.close,
+        "high": crsp.close,
+        "low": crsp.close,
+        "volume": 0,
+        "ret": 0})
+    # Calculate market capitalization
+    crsp["cap"] = crsp.close * crsp.shrout
+    # Shift market capitalization to avoid look ahead bias
+    crsp["cap"] = crsp.groupby("permno").cap.shift(1)
+    # Calculate market capiticalization weight
+    crsp["w_cap"] = crsp.groupby("date").cap.apply(lambda x: x / x.sum())
+    # Convert certain data types to int64
+    crsp = crsp.astype({"permno":"int",
+                        "shrout": "int",
+                        "volume":"int"})
     return crsp
+
+def get_fama_french():
+    """
+    Get 3-factor and momentum data from Ken French data library.
+    """
+    import pandas_datareader as web
+
+    # Transfrom from percentage to nominal value
+    factor = web.DataReader("F-F_Research_Data_Factors_daily", "famafrench", start="1994-01-01", end="2019-12-31")[0]/100
+    momentum = web.DataReader("F-F_Momentum_Factor_daily", "famafrench", start="1994-01-01", end="2019-12-31")[0]/100
+    # Merge into 4 factor model
+    fama_french = pd.concat([factor, momentum], axis=1)
+    fama_french = fama_french.rename(columns=lambda x: x.lower().strip().replace("-", "")).rename_axis(index=str.lower)
+    return fama_french
